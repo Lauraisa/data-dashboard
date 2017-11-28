a4bf16308ae5c1ba3cc42922228e1bb215e5ec07
mirelatv <noelia.mirela@gmail.com>
2017-11-28 17:24:06 -0500

    data

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-window.addEvenListener('load',function(){
+/*window.addEvenListener('load', function() {
+
+  var sedeLima = document.getElemetbyId('sedeLima');
+  var inscritos = document.getElementById('inscritos');
+  var desertores = document.getElementById('desertores');
+  var calificados = document.getElementById('calificados');
+  var calificadosPorcentaje = document.getElemetbyId('calificadosPorcentaje')
+
+sedeLima.addEvenListener('click',function(event){
+
+if  (event.target){
+
+  inscritos.value=console.log(lima['name']);
+}
+
+
+
+
+
+});*/
 
 
 
@@ -12,8 +31,6 @@ window.addEvenListener('load',function(){
 
 
 
-
-})
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
